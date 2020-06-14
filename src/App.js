26280e523f7fe93461ad97a51a3bb0e51e41d2db
mfarzamalam <farzualam9999@gmail.com>
2020-06-14 18:57:35 +0500

    React fragment in replacement for div

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Fragment} from 'react';
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <Fragment>
+        
+    //     <h1>starting</h1>
+    //     <h2>from react fragment</h2>    
+
+    // </Fragment>
+
+    <React.Fragment>
+        <h1>Creating with react fragment</h1>   {/* it is same as div  */}
+        
+    </React.Fragment>
+
   );
 }
 
